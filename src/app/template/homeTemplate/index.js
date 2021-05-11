b44137b2f4c572181f856a8cb4e5c3ecb86f32b3
manhdat <vomanhdat10998@gmail.com>
2021-05-11 08:19:23 +0700

    commit code

--- src/app/template/homeTemplate/index.js
+++ src/app/template/homeTemplate/index.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
+import warningPic from "assets/images/warning.jpg";
 
 import "./styles/styles.scss";
 
@@ -12,11 +13,32 @@ export const HomeTemplate = ({ Component, ...restProps }) => {
       {...restProps}
       render={(propsRoute) => {
         return (
-          <div className="bg-gradient">
-            <Navbar />
-            <Component {...propsRoute} />
-            <Footer />
-          </div>
+          <>
+            <div className="bg-gradient">
+              <Navbar />
+              <Component {...propsRoute} />
+              <Footer />
+            </div>
+
+            <div className="modal-dark">
+              <div className="container img-warning">
+                <div
+                  className="row"
+                  style={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <div className="col-lg-8 col-md-10 col-11">
+                    <img
+                      src={warningPic}
+                      alt={warningPic}
+                      className="img-fluid"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
         );
       }}
     />
