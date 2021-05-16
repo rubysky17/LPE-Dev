9cef0d539422b26b8cc1898d5927b6c29af17ea6
manhdat <vomanhdat10998@gmail.com>
2021-05-16 13:55:49 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import { HomeTemplate } from "app/template/homeTemplate";
 
-// import {
-//   publicRoutes,
-//   privateRoutes,
-//   publicPaths,
-//   privatePaths,
-// } from "../src/app/config/routes/routes";
-
-import ErrorPage from "./app/page/error/";
 import CourseOne from "./app/page/courseOne/";
 
 function App() {
   return (
-    
     <Router>
       <Switch>
         <HomeTemplate exact Component={CourseOne} />
-
-        <Route path="*">
-          <Switch>
-            <Route component={ErrorPage} />
-          </Switch>
-        </Route>
       </Switch>
     </Router>
-
   );
 }
 
