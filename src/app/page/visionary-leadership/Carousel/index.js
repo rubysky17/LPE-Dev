dc3972bc9ab1431cb80e14d3fe4ab5f4c2750f50
manhdat <vomanhdat10998@gmail.com>
2021-05-28 08:43:34 +0700

    [FIX]: visinary node module

--- src/app/page/visionary-leadership/Carousel/index.js
+++ src/app/page/visionary-leadership/Carousel/index.js
@@ -1,26 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import slider01 from "assets/images/slider/slider-01.jpeg";
 
 import "./styles/styles.scss";
 
 function Carousel() {
-
   function imageLoaded(event) {
     event.target.style.opacity = 1;
   }
 
   return (
     <div className="wrapper-slider">
-        <div className="carousel-inner">
-          <div className="item active">
-            <img
-              src={slider01}
-              alt={slider01}
-              onLoad={imageLoaded}
-            />
-          </div>
+      <div className="carousel-inner">
+        <div className="item active">
+          <img src={slider01} alt={slider01} onLoad={imageLoaded} />
         </div>
       </div>
+    </div>
   );
 }
 
