099e0973d6b6dce352987a55008c3b79430c9753
manhdat <vomanhdat10998@gmail.com>
2021-05-28 08:03:48 +0700

    [FIX]: image loaded

--- src/app/page/visionary-leadership/Carousel/index.js
+++ src/app/page/visionary-leadership/Carousel/index.js
@@ -1,37 +1,26 @@
 import React, { useState } from "react";
-import "./styles/styles.scss";
 import slider01 from "assets/images/slider/slider-01.jpeg";
-import Loading from "../Loading";
+
+import "./styles/styles.scss";
 
 function Carousel() {
-  const [onLoad, setOnLoad] = useState(false);
+
+  function imageLoaded(event) {
+    event.target.style.opacity = 1;
+  }
 
   return (
     <div className="wrapper-slider">
-      <div id="myCarousel" className="carousel" data-ride="carousel">
-        {!onLoad && (
-          <>
-            <div className="loading">
-              <Loading />
-            </div>
-          </>
-        )}
         <div className="carousel-inner">
           <div className="item active">
             <img
               src={slider01}
               alt={slider01}
-              onLoad={() => {
-                setOnLoad(true);
-              }}
-              style={{
-                display: !onLoad && "none",
-              }}
+              onLoad={imageLoaded}
             />
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
