60b57ca0158cfee5892509af32dcb76f3973478a
rubysky17 <vomanhdat10998@gmail.com>
2021-06-25 14:48:20 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { HomeTemplate } from "app/template/homeTemplate";
-
-import CourseOne from "./app/page/courseOne/";
 
 function App() {
   return (
